96bb20ee3f8f3b2c5080e40218f5de830c120bd5
Nishat Nasir <nishatnasir00@gmail.com>
2021-04-07 19:16:31 +0000

    Initial commit

---
+++ ques2.cpp
@@ -0,0 +1,46 @@
+// #include <iostream>
+// using namespace std;
+
+// class Calc
+// {
+//   private:
+//     int a, b;
+//   public:
+//     void get();
+//     void sum();
+//     void difference();
+//     void product();
+//     void division();
+// };
+
+// inline void Calc :: get(){
+//   cout << "Enter first Value: "<<endl;
+//   cin >>a;
+//   cout << "Enter Second Value: "<<endl;
+//   cin >>b;
+// }
+
+// inline void Calc :: sum(){
+//   cout <<"Addition of two numbers: "<<a+b <<endl; 
+// }
+
+// inline void Calc :: difference(){
+//   cout <<"Difference of two numbers: "<<a-b <<endl; 
+// }
+
+// inline void Calc :: product(){
+//   cout <<"Product of two numbers: "<<a*b <<endl; 
+// }
+
+// inline void Calc :: division(){
+//   cout <<"Division of two numbers: "<<a/b <<endl;
+// }
+
+// int main(){
+//   Calc math;
+//   math.get();
+//   math.sum();
+//   math.difference();
+//   math.product();
+//   math.division();
+// }
---
+++ ques3.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+
+// class LargestNumber
+// {
+//   private:
+//     int a, b, c;
+//   public:
+//     void input();
+//     void display();
+//     LargestNumber(int x, int y, int z);
+// };
+
+// LargestNumber :: LargestNumber(int x, int y, int z){
+//   a=x;
+//   b=y;
+//   c=z;
+// }
+
+// inline void LargestNumber::input(){
+//   cin >> a >> b >> c;
+// }
+
+// inline void LargestNumber::display(){
+//   int x = a>b? a:b>c? b:c;
+//   cout<< x <<" is the largest number";
+// }
+
+// int main() {
+//   LargestNumber big(10,30,300);
+//   // big.input();
+//   big.display();
+// }
