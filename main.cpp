5ae37c315c21678b2e2e92f7a7dcd36d32fbb2de
Nishat Nasir <nishatnasir00@gmail.com>
2021-04-13 08:28:22 +0000

    edited

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class LargestNumber
-// {
-//   private:
-//     int a, b, c;
-//   public:
-//     void input();
-//     void display();
-// };
+class LargestNumber
+{
+  private:
+    int a, b, c;
+  public:
+    void input();
+    void display();
+};
 
-// inline void LargestNumber::input(){
-//   cin >> a >> b >> c;
-// }
+inline void LargestNumber::input(){
+  cin >> a >> b >> c;
+}
 
-// inline void LargestNumber::display(){
-//   int x = a>b? a:b>c? b:c;
-//   cout<< x <<" is the largest number";
-// }
+inline void LargestNumber::display(){
+  int x = a>b? a:b>c? b:c;
+  cout<< x <<" is the largest number";
+}
 
-// int main() {
-//   LargestNumber big;
-//   big.input();
-//   big.display();
-// }
+int main() {
+  LargestNumber big;
+  big.input();
+  big.display();
+}
--- ques4.cpp
+++ ques4.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class Constructor
-{
-  public:
-    Constructor();
-    Constructor(int a);
-    Constructor(int a, int b);
-};
+// class Constructor
+// {
+//   public:
+//     Constructor();
+//     Constructor(int a);
+//     Constructor(int a, int b);
+// };
 
-Constructor::Constructor(){
-  cout <<"Hello World"<<endl;
-}
+// Constructor::Constructor(){
+//   cout <<"Hello World"<<endl;
+// }
 
-Constructor::Constructor(int a){
-  a/2==0? cout<<a<<" is even" : cout <<a<<" is odd."<<endl;
-}
+// Constructor::Constructor(int a){
+//   a/2==0? cout<<a<<" is even" : cout <<a<<" is odd."<<endl;
+// }
 
-Constructor::Constructor(int a, int b){
-  cout <<a+b;
-}
+// Constructor::Constructor(int a, int b){
+//   cout <<a+b;
+// }
 
-int main(){
-  Constructor obj, obj2(12), obj3(12,10);
+// int main(){
+//   Constructor obj, obj2(12), obj3(12,10);
   
-}
+// }
